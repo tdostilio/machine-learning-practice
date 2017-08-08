cb3d56b834d89482b0adbb43cd747d30527955c2
tdostilio <tdostilio@gmail.com>
2017-08-08 11:39:28 -0400

    wrote function to dress data

--- index.js
+++ index.js
@@ -3,6 +3,43 @@ const csv = require('csvtojson');
 const SLR = ml.SLR; // SIMPLE LINEAR REGRESSION
 
 const csvFilePath = 'advertising.csv'; 
-let csvData = [];
-let x = [];
-let y = [];
+let csvData = [];  //parsed Data
+let x = []; //Input
+let y = []; //Output
+
+let regressionModel;
+
+
+csv()
+    .fromFile(csvFilePath)
+    .on('json', (jsonObj) => {
+        csvData.push(jsonObj);
+    })
+    .on('done', () => {
+        dressData(); // To get data points from JSON Objects
+        performRegression();
+    })
+
+
+function dressData() {
+    /**
+     * One row of the data object looks like:
+     * {
+     *   TV: "10",
+     *   Radio: "100",
+     *   Newspaper: "20",
+     *   "Sales": "1000"
+     * }
+     *
+     * Hence, while adding the data points,
+     * we need to parse the String value as a Float.
+     */
+        csvData.forEach((row) => {
+            x.push(f(row.Radio));
+            y.push(f(row.Sales));
+        });
+}
+
+function f(s) {
+    return parseFloat(s)
+}
